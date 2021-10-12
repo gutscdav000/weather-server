8a3366b84784cdfd7cdcbbc10f25870a8153a46c
David Gutsch <gutscdav000@gmail.com>
2021-10-12 11:09:21 -0400

    Updating endpoint being called to retrieve Alerts as well.

--- src/main/scala/com/jackhenry/weatherserver/Forecasts.scala
+++ src/main/scala/com/jackhenry/weatherserver/Forecasts.scala
@@ -20,21 +20,26 @@ trait Forecasts[F[_]]{
 object Forecasts {
   def apply[F[_]](implicit ev: Forecasts[F]): Forecasts[F] = ev
 
-  final case class ForecastResponse(condition: String, temperature: Double, alert: Option[String])
+  final case class ForecastResponse(condition: String, temperature: Double, alert: String)
   final case class Weather(id: Int, main: String, description: String, icon: String)
+  final case class Alert(sender_name: String, event: String, start: Int, end: Int, description: String, tags: List[String])
   final case class ForecastError(e: Throwable) extends RuntimeException
 
   object ForecastResponse {
     implicit val decoder: Decoder[ForecastResponse] = (hCursor: HCursor) =>
       for {
-        weather <- hCursor.downField("weather").as[List[Weather]]
+        weather <- hCursor.downField("current").downField("weather").as[List[Weather]]
         condition = weather.headOption match {
           case Some(weath) => weath.main + ": "+ weath.description
           case None => "No Weather Condition Retrieved"
         }
-        temperature <- hCursor.downField("main").downField("temp").as[Double]
-        //    alert <- hCursor.get[String]("")
-      } yield ForecastResponse(condition, temperature, None)
+        temperature <- hCursor.downField("current").downField("temp").as[Double]
+        alerts <- hCursor.downField("alerts").as[Option[List[Alert]]]
+        parsedAlerts: String = alerts match {
+          case Some(als) => als.map(a => a.sender_name + ": " + a.event).reduce((s1, s2) => s1 + "\n" + s2)
+          case None => "No current weather Alerts"
+        }
+      } yield ForecastResponse(condition, temperature, parsedAlerts)
     implicit def entityDecoder[F[_]: Concurrent]: EntityDecoder[F, ForecastResponse] = jsonOf
 
     implicit val forecastEncoder: Encoder[ForecastResponse] = deriveEncoder[ForecastResponse]
@@ -47,11 +52,12 @@ object Forecasts {
     import dsl._
     def get(lat: Double, long: Double, appId: String): F[Forecasts.ForecastResponse] = {
       C.expect[ForecastResponse](GET(
-        uri"https://api.openweathermap.org/data/2.5/weather"
+        uri"https://api.openweathermap.org/data/2.5/onecall"
           .withQueryParam("lat", lat)
           .withQueryParam("lon", long)
           .withQueryParam("appid", appId)
           .withQueryParam("units", "imperial")
+          .withQueryParam("exclude", "minutely,hourly,daily")
         ,
         Accept(MediaType.application.json)
       ))
